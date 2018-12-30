de2c07441aa880801b00f0e88256ec8af42eff95
providence97 <ptphuong@example.com>
2018-12-30 10:01:39 +0700

    Chinh sua cart contact

--- routes/cart.js
+++ routes/cart.js
@@ -36,7 +36,31 @@ router.get('/', function (req, res, next) {
     }
 });
 
-
+router.get('/purchase', function (req, res, next) {
+    if (req.session.cart.length == 0) {
+        res.redirect('/cart');
+    } else {
+        var cart = req.session.cart;
+        productDAO.loadByCart(cart).then(result => {
+            var total = 0;
+            for (var i = 0; i < result.length; i++) {
+                result[i].gia_f = priceFormat(result[i].gia);
+                for (var j = 0; j < cart.length; j++) {
+                    if (result[i].idsanpham == cart[j].id) {
+                        result[i].quantity = cart[j].quantity;
+                    }
+                }
+                total += result[i].gia * result[i].quantity;
+            }
+            res.render('purchase/purchase', {
+                title: 'Thanh toán đơn hàng | CamShop',
+                money: total,
+                money_f: priceFormat(total),
+                defaultName: req.session.user.hoten
+            });
+        });
+    }
+});
 
 router.post('/purchase', function (req, res, next) {
     var orderId = randomOrder();
