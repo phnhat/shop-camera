837164666a004ae4c665552dce56e899ec6a9ced
providence97 <ptphuong@example.com>
2019-01-01 21:27:19 +0700

    Pagination

--- routes/search.js
+++ routes/search.js
@@ -26,6 +26,29 @@ router.get('/list', function (req, res, next) {
                         result[i].gia_f = priceFormat(result[i].gia);
                     }
 
+                    // --- Phân trang ---
+                    var getUrl = ``;
+                    if (params.name != undefined)
+                        getUrl += `name=${params.name}&`;
+                    if (params.brand != undefined)
+                        getUrl += `brand=${params.brand}&`;
+                    if(params.type != undefined)
+                        getUrl += `type=${params.type}&`;
+                    if(params.nation != undefined)
+                        getUrl += `nation=${params.nation}&`;
+                    if(params.min != undefined)
+                        getUrl += `min=${params.min}&`;
+                    if(params.max != undefined)
+                        getUrl += `max=${params.max}&`;
+                    var _pages = ``;
+                    for (var j = 1; j <= nPages; j++) {
+                        if (j == currentPage) {
+                            _pages += `<li class="active"> <a href="#"> ${j} </a> </li>`;
+                        } else {
+                            _pages += `<li> <a href="/search/list?${getUrl}page=${j}"> ${j} </a> </li>`;
+                        }
+                    }
+                    // ------------------
 
                     res.render('product/list', {
                         title: 'Kết quả tìm kiếm | CamShop',
