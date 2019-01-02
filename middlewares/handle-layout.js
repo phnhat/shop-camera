207cf2ee24f5bcff84215944114d134e771d47a5
in30Minutes <kelvinphamit@gmail.com>
2019-01-02 13:29:20 +0700

    update handle-layout.js

--- middlewares/handle-layout.js
+++ middlewares/handle-layout.js
@@ -10,7 +10,7 @@ module.exports = function (req, res, next) {
                 isAdmin: req.session.isAdmin,
                 user: req.session.user
             }
-            next();
+            next()
         });
     });
 }
