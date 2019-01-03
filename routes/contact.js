0b53b9bbc918690bedce193e203577674243bd11
providence97 <ptphuong@example.com>
2019-01-03 14:20:28 +0700

    Sua index contact

--- routes/contact.js
+++ routes/contact.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', (req, res, next) => {
-    res.render('contact', {
-        title: 'Liên hệ | CamShop'
+router.get('/', function (req, res, next) {
+
+} else {
+    var cart = req.session.cart;
+    productDAO.loadByCart(cart).then(result => {
+        var total = 0;
+       
+        res.render('contact', {
+            title: 'Contact | CamShop',
+            empty: false,
+            products: result,
+            money: total,
+            money_f: priceFormat(total)
+        });
     });
+}
 });
 
 module.exports = router;
