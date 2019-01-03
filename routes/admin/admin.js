bc63d70a7bcae319de622b9161e8a1ca87c40979
Phat Nguyen <phatnt1995@gmail.com>
2019-01-03 16:31:36 +0700

    Add types router:
    - Add get all types
    - Add add a new type

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -1,11 +1,13 @@
 var productDAO = require('../../database/productDAO');
 
 var mnBrands = require('./mn-brands');
+var mnTypes = require('./mn-types');
 
 var express = require('express');
 var router = express();
 
 router.use('/quanlythuonghieu', mnBrands);
+router.use('/quanlyloai', mnTypes);
 
 router.get('/', function (req, res, next) {
     loadAdminDashboard(res);
@@ -18,10 +20,12 @@ router.get('/home', function (req, res, next) {
 async function loadAdminDashboard(res) {
 
     var brands = await productDAO.loadAllBrands();
+    var types = await productDAO.loadAllTypes();
 
     res.render('admin/dashboard', {
         layout: 'admin/layout',
         nBrands: brands.length,
+        nTypes: types.length,
     });
 
 }
