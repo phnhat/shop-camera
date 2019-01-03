36020e265bdab884a951d57a1d9bbb1ac512b0b4
Phat Nguyen <phatnt1995@gmail.com>
2019-01-03 14:20:06 +0700

    Update password

--- routes/account.js
+++ routes/account.js
@@ -24,3 +24,37 @@ router.post('/', function (req, res, next) {
     });
 
 });
+
+router.get('/password', function (req, res, next) {
+    res.render('account/password', {
+        title: 'Đổi mật khẩu | CamShop',
+        succ: req.session.change_password_succ,
+        fail: req.session.change_password_fail
+    });
+
+    if (req.session.change_password_succ != undefined) {
+        delete req.session.change_password_succ;
+    }
+
+    if (req.session.change_password_fail != undefined) {
+        delete req.session.change_password_fail;
+    }
+
+});
+
+router.post('/password', function (req, res, next) {
+    var old_password = md5(req.body.old_password);
+    if(old_password == req.session.user.matkhau) {
+        var new_password = md5(req.body.new_password);
+        accountDAO.changePassword(req.session.user.email, new_password).then(result => {
+            req.session.change_password_succ = true;
+            req.session.user.matkhau = new_password;
+            res.redirect('/account/password');
+        });
+    } else {
+        req.session.change_password_fail = true;
+        res.redirect('/account/password');
+    }
+});
+
+module.exports = router;
