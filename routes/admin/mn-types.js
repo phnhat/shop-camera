6d1152a68f4904f9ab0f27b84ffa435b6172e3a2
Phat Nguyen <phatnt1995@gmail.com>
2019-01-04 17:08:28 +0700

    Add update a type with id and remove a type with id in types router

--- routes/admin/mn-types.js
+++ routes/admin/mn-types.js
@@ -23,4 +23,27 @@ router.post('/add', async function (req, res, next) {
     }
 });
 
+router.post('/update', async function (req, res, next) {
+    var id = req.body.id;
+    var name = req.body.name;
+    try {
+        var x = await productDAO.updateType(id, name);
+        res.send('Cập nhật thành công.')
+    } catch (error) {
+        res.send('Cập nhật thất bại.');
+    }
+});
+
+router.post('/remove', async function (req, res, next) {
+    var id = req.body.id;
+    console.log(req.body);
+    try {
+        var x = await productDAO.removeType(id);
+        res.send('Xóa loại thành công.')
+    } catch (error) {
+        console.log(error);
+        res.send('Xóa loại thất bại vì loại này tồn tại trong các bảng dữ liệu khác.');
+    }
+});
+
 module.exports = router;
