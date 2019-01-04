65cbfa7390d9992c5b2cd78c536bb5e769273175
in30Minutes <kelvinphamit@gmail.com>
2019-01-04 09:39:33 +0700

    final accountDAO.js

--- database/accountDAO.js
+++ database/accountDAO.js
@@ -4,6 +4,7 @@ module.exports.loadAll = function() {
     return db.executeQuery('SELECT * FROM taikhoan;');
 }
 
+//=> Them tai khoan
 module.exports.addUser = function(email, password, name) {
     var sql = `INSERT INTO taikhoan(email, matkhau, hoten) VALUES ("${email}", "${password}", "${name}");`;
     return db.executeQuery(sql);
@@ -14,12 +15,13 @@ module.exports.getUser = function(email) {
     return db.executeQuery(sql);
 }
 
-
+//doi mat khau
 module.exports.changePassword = function(email, password) {
     var sql = ` UPDATE taikhoan SET matkhau = "${password}" where email = "${email}" `;
     return db.executeQuery(sql);
 }
 
+//thay doi thong tin tai khoan
 module.exports.updateUser = function(email, name, phoneNumber){
 	var sql = `UPDATE taikhoan SET hoten = "${name}", sdt="${phoneNumber}" WHERE email = "${email}"`;
 	return db.executeQuery(sql);
